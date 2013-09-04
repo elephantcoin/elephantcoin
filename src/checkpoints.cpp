2401acf5c94f7fd0c22901073ee7b995dec9a567
elephantcoin <ec92386593@yahoo.com>
2013-09-04 14:01:37 -0700

    modified:   src/checkpoints.cpp 	new file:   src/qt/res/icons/tx_mined2.png 	new file:   src/qt/res/icons/tx_mined3.png

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -16,24 +16,19 @@ namespace Checkpoints
 {
     typedef std::map<int, uint256> MapCheckpoints;
 
-    //
-    // What makes a good checkpoint block?
-    // + Is surrounded by blocks with reasonable timestamps
-    //   (no blocks before with a timestamp after, none after with
-    //    timestamp before)
-    // + Contains no strange transactions
-    //
-
 	// no checkpoint now, can be added in later releases
     static MapCheckpoints mapCheckpoints =
             boost::assign::map_list_of
-            (    1, uint256("0xa9ad33dce143c0074122e84786e715e44226df070baa1ffb0a74c325e035114a"))
-            (  101, uint256("0x5555b57a8b8f109766c5160a030dc5dc3f3ec3132c1094bc677b32dfa64a3a5f"))
-            ( 1001, uint256("0x56e74fe30a2f825ebb62705e5a40862ff5c4509feea97e3853270075624f6156"))
-            (10001, uint256("0x910c90a245fd8be594c7d025b8b8beb7336048f2e6b0b96c21a863aaa071ae5a"))
-            (20001, uint256("0x3f777e38449751c2d160ac50e15a57de4c9c6c26c405112c6c5219f68c2e896c"))
-			(25000, uint256("0xbab7747ccd58c78c55549189235a842c27591128b9a2d062bc572a081b4b5308"))
-			(36492, uint256("0x2458199c5fbba8b65a004c1bede5c6af9b5e562baac7105b7731bcc077684435"))
+            (     1, uint256("0xa9ad33dce143c0074122e84786e715e44226df070baa1ffb0a74c325e035114a"))
+            (   101, uint256("0x5555b57a8b8f109766c5160a030dc5dc3f3ec3132c1094bc677b32dfa64a3a5f"))
+            ( 10001, uint256("0x910c90a245fd8be594c7d025b8b8beb7336048f2e6b0b96c21a863aaa071ae5a"))
+			( 36492, uint256("0x2458199c5fbba8b65a004c1bede5c6af9b5e562baac7105b7731bcc077684435"))
+			( 89136, uint256("0x6087a054586c304c2cd6f1ee282747bea309ccbeaaf1b114c411c761ac91b7ae"))
+			(100001, uint256("0xe23d8facfdea566e464255b03f933632a21612da7a2643d4155ecdeb476afc44"))
+			(137448, uint256("0xd83b43497b9f74d821cb5c575f4f71392c5db1facdbd7b4b514f3f996c2de0b2"))
+			(178549, uint256("0x70710816bd11aa9f9643b21e23b3708240b0ec2dadcac7d96ddd1474cc251bfd"))
+			(200001, uint256("0x8378dc13be41283851554004cfdbe24e39fc94f55df582e6ed7cc6703e1f701f"))
+			(217533, uint256("0xe95b6afc14318ff39f69a4ac931ea3a1b75b693e4d899478bba6ff622da597a5"))
 			;
 
 
